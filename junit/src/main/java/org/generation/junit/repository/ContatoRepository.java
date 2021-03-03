605e8b11ea9f5c15df5dabc844783799187cdc02
AMS <alonso_mendess@mail.com>
2021-03-03 20:50:42 -0300

    atualização

--- junit/src/main/java/org/generation/junit/repository/ContatoRepository.java
+++ junit/src/main/java/org/generation/junit/repository/ContatoRepository.java
@@ -3,6 +3,5 @@ package org.generation.junit.repository;
 import org.generation.junit.model.ContatoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface ContatoRepository extends JpaRepository<ContatoModel, Long> {
 }
